73e86459e17ce8ba970498f3379f94480de5589a
kahlaoui-2020 <61467536+kahlaoui-2020@users.noreply.github.com>
2021-03-04 09:43:16 +0100

    Update app.component.ts

--- clientMenuCinema/src/app/app.component.ts
+++ clientMenuCinema/src/app/app.component.ts
@@ -10,16 +10,7 @@ import * as firebase from 'firebase'
 export class AppComponent {
 
   constructor(private tmdb: TmdbService) {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBCEM2PkW9y4tI0jMB0N2iehMmbT_7apeY",
-      authDomain: "menucinema-fd117.firebaseapp.com",
-      databaseURL: "https://menucinema-fd117.firebaseio.com",
-      projectId: "menucinema-fd117",
-      storageBucket: "menucinema-fd117.appspot.com",
-      messagingSenderId: "838892627178",
-      appId: "1:838892627178:web:225dd5af3fb7e3c4086393",
-      measurementId: "G-XYWPKQM9WN"
-    };
+    const firebaseConfig = {};
     // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
